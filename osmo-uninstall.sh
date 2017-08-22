feeedaddd6eb51ebbb7ff688008148ad644db187
Neels Hofmeyr <neels@hofmeyr.de>
2017-08-23 00:13:33 +0200

    add osmo-uninstall.sh

---
+++ osmo-uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Remove all osmocom installations from given prefix, default is /usr/local
+prefix="${1-/usr/local}"
+echo "$prefix"
+rm -rf $prefix/lib/libasn1c*
+rm -rf $prefix/lib/libgtp.*
+rm -rf $prefix/lib/libmtp.*
+rm -rf $prefix/lib/libosmo*
+rm -rf $prefix/lib/libsccp.*
+rm -rf $prefix/lib/libsmpp34*
+rm -rf $prefix/lib/libxua.*
+rm -rf $prefix/lib/pkgconfig/
+rm -rf $prefix/include/asn1c/
+rm -rf $prefix/include/gtp.h
+rm -rf $prefix/include/gtpie.h
+rm -rf $prefix/include/openbsc/
+rm -rf $prefix/include/osmocom/
+rm -rf $prefix/include/pdp.h
+rm -rf $prefix/include/smpp34*
+rm -rf $prefix/include/def_*
+rm -rf $prefix/bin/abisip-find
+rm -rf $prefix/bin/asn1c
+rm -rf $prefix/bin/bs11_config
+rm -rf $prefix/bin/crfc2asn1.pl
+rm -rf $prefix/bin/enber
+rm -rf $prefix/bin/ggsn
+rm -rf $prefix/bin/ipaccess-config
+rm -rf $prefix/bin/ipaccess-proxy
+rm -rf $prefix/bin/isdnsync
+rm -rf $prefix/bin/meas_json
+rm -rf $prefix/bin/osmo-*
+rm -rf $prefix/bin/sgsnemu
+rm -rf $prefix/bin/unber
