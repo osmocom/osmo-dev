c063863f2632e9f073304486e9882ad671e76013
Oliver Smith <osmith@sysmocom.de>
2019-11-14 13:58:02 +0100

    add chat handler that prints the message
    Change-Id: I7a408a978df2244777c5e11a3eb421e30612f1e6

--- net/templates/freeswitch/python/dialplan-dgsm/__init__.py
+++ net/templates/freeswitch/python/dialplan-dgsm/__init__.py
@@ -1,29 +1,29 @@
-import argparse
 import json
 import subprocess
 
 
-def query_mslookup(service_type, id, id_type="msisdn"):
-	query_str = '{}.{}.{}'.format(service_type, id, id_type)
+def query_mslookup(service_type, id, id_type='msisdn'):
+	query_str = '%s.%s.%s' % (service_type, id, id_type)
+	print('[dialplan-dgsm] mslookup: ' + query_str)
+
 	result_line = subprocess.check_output([
 		'osmo-mslookup-client', query_str, '-f', 'json'])
 	if isinstance(result_line, bytes):
 		result_line = result_line.decode('ascii')
+
+	print('[dialplan-dgsm] mslookup result: ' + result_line)
 	return json.loads(result_line)
 
 
 def handler(session, args):
 	""" Handle calls: bridge to the SIP server found with mslookup. """
-	print('[dialplan-dgsm] handler')
+	print('[dialplan-dgsm] call handler')
 	msisdn = session.getVariable('destination_number')
-	print('[dialplan-dgsm] resolving: sip.voice.' + str(msisdn) + '.msisdn')
 
 	# Run osmo-mslookup-client binary. We have also tried to directly call the C functions with ctypes but this has
 	# lead to hard-to-debug segfaults.
 	try:
 		result = query_mslookup("sip.voice", msisdn)
-
-		print('[dialplan-dgsm] result: ' + str(result))
 
 		# This example only makes use of IPv4
 		if not result['v4']:
@@ -37,12 +37,34 @@ def handler(session, args):
 
 		session.execute('bridge', dial_str)
 	except:
-		print('[dialplan-dgsm]: could not resolve MSISDN {} with mslookup.py'.format(msisdn))
+		print('[dialplan-dgsm]: exception during call handler')
 		session.hangup('UNALLOCATED_NUMBER')
 
 
-# def chat(message, args):
-#	""" Handle SMS: forward to the SMSC found with mslookup. """
+def chat(message, args):
+	""" Handle SMS: forward to the SMSC found with mslookup. """
+	print('[dialplan-dgsm] SMS handler')
+
+	message.addHeader("final_delivery", "true")
+	source = message.getHeader("from_user")
+	destination = message.getHeader("to_user")
+	text = message.getBody()
+	charset = 'UTF-8'
+	coding = '2'
+
+	print('[dialplan-dgsm] message from %s to %s: %s' % (source, destination, text))
+	try:
+		result = query_mslookup('smpp.sms', destination)
+
+		# This example only makes use of IPv4
+		if not result['v4']:
+			print('[dialplan-dgsm] no IPv4 result from mslookup')
+			return
+
+		smsc_ip, smsc_port = result['v4']
+		# TODO: send the message, best is probably an extra python process
+	except:
+		print('[dialplan-dgsm]: exception during SMS handler')
 
 
 def fsapi(session, stream, env, args):
@@ -51,6 +73,8 @@ def fsapi(session, stream, env, args):
 
 
 def main():
+	import argparse
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument('id', type=int)
 	parser.add_argument('-i', '--id-type', default='msisdn', help='default: "msisdn"')
