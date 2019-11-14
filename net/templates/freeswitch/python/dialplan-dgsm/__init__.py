1033813ea46bfda8cb959356042cb157d878b7ff
Neels Hofmeyr <neels@hofmeyr.de>
2019-11-14 05:59:05 +0100

    use osmo-mslookup-client instead of mslookup.py
    Change-Id: I452c3be872aa0bcec10410af45a517b8fdf10bc9

--- net/templates/freeswitch/python/dialplan-dgsm/__init__.py
+++ net/templates/freeswitch/python/dialplan-dgsm/__init__.py
@@ -3,7 +3,17 @@ import os
 import subprocess
 
 script_dir = os.path.dirname(__file__)
-timeout_ms = 5000
+timeout_ms = 2000
+
+def query_mslookup(query_str):
+	result_line = subprocess.check_output([
+		'osmo-mslookup-client', query_str, '-f', 'json'])
+	if isinstance(result_line, bytes):
+		result_line = result_line.decode('ascii')
+	return json.loads(result_line)
+
+def query_mslookup_msisdn(msisdn):
+	return query_mslookup('sip.voice.%s.msisdn' % str(msisdn))
 
 def handler(session, args):
 	print('[dialplan-dgsm] handler')
@@ -12,30 +22,32 @@ def handler(session, args):
 
 	# Run mslookup.py. In theory, we should be able to import it and call mslookup.resolve() directly, however this
 	# has lead to hard-to-debug segfaults when calling it the second time. For now, use a separate python process.
-	result = {}
 	try:
-		result_json = subprocess.check_output([script_dir + '/mslookup.py', str(msisdn), '-t', str(timeout_ms)])
-		result = json.loads(result_json)
+		result = query_mslookup_msisdn(msisdn)
+
+		print('[dialplan-dgsm] result: ' + str(result))
+
+		# This example only makes use of IPv4
+		if not result['v4']:
+			print('[dialplan-dgsm] no IPv4 result from mslookup')
+			session.hangup('UNALLOCATED_NUMBER')
+			return
+
+		sip_ip, sip_port = result['v4']  # osmo-dev defaults: same as ${SIPCON_LOCAL}
+		dial_str = 'sofia/internal/sip:{}@{}:{}'.format(msisdn, sip_ip, sip_port)
+		print('[dialplan-dgsm] dial_str: ' + str(dial_str))
+
+		session.execute('bridge', dial_str)
 	except:
-		print('[dialplan-dgsm]: failed to run mslookup.py!')
+		print('[dialplan-dgsm]: could not resolve MSISDN {} with mslookup.py'.format(msisdn))
 		session.hangup('UNALLOCATED_NUMBER')
-
-	print('[dialplan-dgsm] result: ' + str(result))
-
-	# This example only makes use of IPv4
-	if not result['v4']:
-		print('[dialplan-dgsm] no IPv4 result from mslookup')
-		session.hangup('UNALLOCATED_NUMBER')
-		return
-
-	mncc_ip = result['v4']['ip']  # osmo-dev defaults: same as ${SIPCON_LOCAL}
-	mncc_port = result['v4']['port']  # osmo-dev defaults: same as ${SIPCON_LOCAL_PORT}
-	dial_str = 'sofia/internal/sip:{}@{}:{}'.format(msisdn, mncc_ip, mncc_port)
-	print('[dialplan-dgsm] dial_str: ' + str(dial_str))
-
-	session.execute('bridge', dial_str)
 
 
 # Freeswitch refuses to load the module without this
 def fsapi(session, stream, env, args):
 	stream.write(env.serialize())
+
+if __name__ == '__main__':
+	import json
+	import sys
+	print(json.dumps(query_mslookup(sys.argv[1])))
