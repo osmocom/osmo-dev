836bf568ef7fb8664395e28a481ecc10a6d2e2cd
Oliver Smith <osmith@sysmocom.de>
2021-08-12 14:08:59 +0200

    ttcn3/wrappers: remove
    Leftover from before the script was refactored in
    Iecba5391dafc353058d369deb513b156534face1.
    
    Change-Id: Ic0890f71fe37a136580e12ebade979bcf14574c0

--- ttcn3/wrappers/osmo-bts-trx-respawn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-# Automatically restart osmo-bts-trx while running TTCN3 tests. See docker-playground.git.
-
-while true; do
-	osmo-bts-trx "$@"
-done
--- ttcn3/wrappers/osmo-pcu-respawn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-# Automatically restart osmo-pcu while running TTCN3 tests. See docker-playground.git's osmo-pcu-master/respawn.sh.
-
-while true; do
-	osmo-pcu "$@"
-done
