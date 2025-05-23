osmocom/osmo-dev

commit 8ea5c0cec661c7d4a4a6b488cac52c48f643be4e

gen_makefile: fix building open5gs
Fix that gen_src_proj_copy() returns a relative path for projects where
src_copy is not needed (either because the option is disabled or because
the project doesn't use autotools, such as open5gs). This leads to
generating the wrong build_to_src dir below, where os.path.relpath is
getting called on the generated src_proj_copy:

  build_to_src = os.path.relpath(src_proj_copy, build_proj)

Fix for:

  $ ./testenv.py run hss --podman
  …
  ===== .make.open5gs.configure

  chmod -R ug+w open5gs
  rm -rf open5gs
  mkdir -p open5gs
  cd open5gs; CFLAGS=-g meson setup ../../../../../../../code/osmo-dev/src/open5gs . \
          --prefix /home/user/.cache/osmo-ttcn3-testenv/podman/usr

  ERROR: Neither directory contains a build file meson.build.
  make: *** [Makefile:4501: .make.open5gs.configure] Error 1

Fixes: 9cf2b98e ("gen_makefile: add --autoreconf-in-src-copy arg")
Change-Id: I6da15a0d825d06a20fe435e3545fa92a27c80cb9

gen_makefile.py
@@ -437,7 +437,7 @@ def gen_update_src_copy_cmd(proj, src_dir, make_dir):
 
 def gen_src_proj_copy(src_proj, make_dir, proj):
   if not is_src_copy_needed(proj):
-    return src_proj
+    return os.path.join(make_dir, src_proj)
   return os.path.join(make_dir, "src_copy", proj)
 
 def gen_make(proj, deps, configure_opts, jobs, make_dir, src_dir, build_dir, url, push_url, sudo_make_install, no_ldconfig, ldconfig_without_sudo, make_check):

commit 9035433af0a9edc0a60d8e12975e4a58a1d5e476

gen_makefile: {make,src}_dir: use absolute paths
Fix generating wrong relative paths when the make dir (-m) is in the
same directory as gen_makefile.py.

This was too easy to get wrong with relative paths as shown in the
previous patches. I've added tests to mitigate this and switched to
absolute paths to make the logic easier to follow.

Fix for:

  $ ./gen_makefile.py -m build
  $ make -C build libosmocore
  …
  cd build/../src/libosmocore; autoreconf -fi
  /bin/sh: 1: cd: can't cd to build/../src/libosmocore

Fixes: 8ea5c0ce ("gen_makefile: fix building open5gs")
Change-Id: I8bb2af7ffb902fd690e83e36739c2b4415b9ab80

contrib/jenkins.sh
@@ -1,0 +1,3 @@
+#!/bin/sh -ex
+
+pytest -v

gen_makefile.py
@@ -437,7 +437,7 @@ def gen_update_src_copy_cmd(proj, src_dir, make_dir):
 
 def gen_src_proj_copy(src_proj, make_dir, proj):
   if not is_src_copy_needed(proj):
-    return os.path.join(make_dir, src_proj)
+    return src_proj
   return os.path.join(make_dir, "src_copy", proj)
 
 def gen_make(proj, deps, configure_opts, jobs, make_dir, src_dir, build_dir, url, push_url, sudo_make_install, no_ldconfig, ldconfig_without_sudo, make_check):
@@ -445,8 +445,6 @@ def gen_make(proj, deps, configure_opts, jobs, make_dir, src_dir, build_dir, url
   if proj == 'openbsc':
     src_proj = os.path.join(src_proj, 'openbsc')
 
-  src = os.path.relpath(src_dir, make_dir)
-  src_proj = os.path.relpath(src_proj, make_dir)
   src_proj_copy = gen_src_proj_copy(src_proj, make_dir, proj)
 
   build_proj = os.path.join(build_dir, proj)
@@ -491,7 +489,7 @@ def gen_make(proj, deps, configure_opts, jobs, make_dir, src_dir, build_dir, url
     -and -not -name "config.h" 2>/dev/null)
 
 {gen_makefile_clone(proj,
-                    src,
+                    src_dir,
                     src_proj,
                     url,
                     push_url,
@@ -558,6 +556,9 @@ make_dir = args.make_dir
 if not make_dir:
   opts_names = '+'.join([f.replace('.opts', '') for f in configure_opts_files])
   make_dir = 'make-%s' % opts_names
+
+make_dir = os.path.abspath(make_dir)
+src_dir = os.path.abspath(args.src_dir)
 
 if not os.path.isdir(make_dir):
   os.makedirs(make_dir)
@@ -693,7 +694,7 @@ for proj, deps in projects_deps:
   all_config_opts.extend(configure_opts.get('ALL') or [])
   all_config_opts.extend(configure_opts.get(proj) or [])
   content += gen_make(proj, deps, all_config_opts, args.jobs,
-                     make_dir, args.src_dir, build_dir, args.url, args.push_url,
+                     make_dir, src_dir, build_dir, args.url, args.push_url,
                      args.sudo_make_install, args.no_ldconfig,
                      args.ldconfig_without_sudo, args.make_check)
 

pytest.ini
@@ -1,0 +1,3 @@
+[pytest]
+testpaths = tests
+addopts = --tb=short

tests/test_gen_makefile.py
@@ -1,0 +1,66 @@
+#!/usr/bin/env python3
+# Copyright 2025 sysmocom - s.f.m.c. GmbH
+# SPDX-License-Identifier: GPL-3.0-or-later
+import os
+import shlex
+import subprocess
+
+osmo_dev_path = os.path.realpath(os.path.join(__file__, "../../"))
+
+
+def run_cmd(cmd, *args, **kwargs):
+    print(f"+ {cmd}")
+    return subprocess.run(cmd, check=True, *args, **kwargs)
+
+
+def run_make_regen_2x(cwd):
+    """The "make regen" command regenerates the Makefile. Ensure that this
+    works twice in a row."""
+    run_cmd(["make", "regen"], cwd=cwd)
+    run_cmd(["make", "regen"], cwd=cwd)
+
+
+def put_gen_makefile_into_tmp_path(tmp_path):
+    """Ensure running gen_makefile.py -m=<relative path> works as expected."""
+    run_cmd(f"cp -v *.* {shlex.quote(str(tmp_path))}", cwd=osmo_dev_path, shell=True)
+    run_cmd(["ln", "-s", os.path.join(osmo_dev_path, "src"), os.path.join(tmp_path, "src")])
+
+
+def test_make_libosmocore_autoconf(tmp_path):
+    run_cmd(["./gen_makefile.py", "default.opts", "iu.opts", "no_dahdi.opts", "-m", tmp_path], cwd=osmo_dev_path)
+    run_cmd(["make", ".make.libosmocore.autoconf"], cwd=tmp_path)
+    run_make_regen_2x(tmp_path)
+
+
+def test_make_libosmocore_autoconf_src_copy(tmp_path):
+    run_cmd(["./gen_makefile.py", "-m", tmp_path, "--autoreconf-in-src-copy"], cwd=osmo_dev_path)
+    run_cmd(["make", ".make.libosmocore.autoconf"], cwd=tmp_path)
+    run_make_regen_2x(tmp_path)
+
+
+def test_make_libosmocore_autoconf_relative_makedir(tmp_path):
+    put_gen_makefile_into_tmp_path(tmp_path)
+    run_cmd(["./gen_makefile.py", "-m", "make"], cwd=tmp_path)
+    make_path = os.path.join(tmp_path, "make")
+    run_cmd(["make", ".make.libosmocore.autoconf"], cwd=make_path)
+    run_make_regen_2x(make_path)
+
+
+def test_make_libosmocore_autoconf_relative_makedir_src_copy(tmp_path):
+    put_gen_makefile_into_tmp_path(tmp_path)
+    run_cmd(["./gen_makefile.py", "-m", "make", "--autoreconf-in-src-copy"], cwd=tmp_path)
+    make_path = os.path.join(tmp_path, "make")
+    run_cmd(["make", ".make.libosmocore.autoconf"], cwd=make_path)
+    run_make_regen_2x(make_path)
+
+
+def test_make_open5gs_configure(tmp_path):
+    run_cmd(["./gen_makefile.py", "-m", tmp_path], cwd=osmo_dev_path)
+    run_cmd(["make", ".make.open5gs.configure"], cwd=tmp_path)
+    run_make_regen_2x(tmp_path)
+
+
+def test_make_open5gs_configure_src_copy(tmp_path):
+    run_cmd(["./gen_makefile.py", "-m", tmp_path, "--autoreconf-in-src-copy"], cwd=osmo_dev_path)
+    run_cmd(["make", ".make.open5gs.configure"], cwd=tmp_path)
+    run_make_regen_2x(tmp_path)