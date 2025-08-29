osmocom/osmo-dev

commit 1d13c9d70a38a6e5d7ce8c3575b00aacc0f0c52b

gen_makefile: add --targets
Add an option to specify the targets to include in the Makefile, instead
of always including all of them. This option will be used by testenv to
check early if there are any unknown targets, and to make running the
generated Makefile faster.

Running the Makefile is currently slowed down when having a lot of
projects cloned in osmo-dev's src dir, because regardless of the target
to be built, make is looking for all source files for all projects while
parsing the Makefile. For each project we have:

  libosmocore_configure_files := $(shell find -L …/src/libosmocore …)
  libosmocore_files := $(shell find -L …/src/libosmocore …)

Changing ":=" to "=" doesn't make it faster either, make still executes
the find commands while parsing target lines such as:

  .make.libosmocore.build: .make.libosmocore.configure $(libosmocore_files)

Related: osmo-ttcn3-hacks Ib2d8802b305f145d27aab3c1cc3129709b93d93d
Change-Id: I89bb60e94dd03606dbba5a3609d5e1a95993af5b

gen_makefile.py
@@ -153,6 +153,9 @@ parser.add_argument('-i', '--install-prefix', default='/usr/local',
 parser.add_argument('-A', '--autoreconf-in-src-copy', action='store_true',
                     help="run autoreconf in a copy of the source dir, avoids 'run make distclean' errors")
 
+parser.add_argument('--targets',
+                    help="comma separated list of high-level targets to build instead of all targets")
+
 args = parser.parse_args()
 
 class listdict(dict):
@@ -213,6 +216,40 @@ def gen_convenience_targets():
       ret += "\n"
     ret += f".PHONY: {short}\n"
     ret += f"{short}: {' '.join(full)}\n"
+  return ret
+
+def filter_projects_deps_targets():
+  if not args.targets:
+    return projects_deps
+
+  ret = {}
+  for target in args.targets.split(","):
+    # .make.osmocom-bb.clone -> osmocom-bb
+    if target.startswith(".make."):
+      target = target.split(".")[2]
+
+    current_targets = [target]
+    if target in convenience_targets:
+      current_targets = convenience_targets[target]
+
+    for target in current_targets:
+      # Add target + all dependencies to ret
+      queue = [target]
+      while queue:
+        project = queue.pop()
+        if project not in projects_deps:
+          print()
+          print(f"ERROR: filter_projects_deps_targets: can't find project {project} in projects_deps!")
+          print()
+          sys.exit(1)
+
+        deps = projects_deps[project]
+        ret[project] = deps
+
+        for dep in deps:
+          if dep not in ret:
+            queue += [dep]
+
   return ret
 
 def gen_makefile_clone(proj, src, src_proj, update_src_copy_cmd):
@@ -555,6 +592,7 @@ def gen_make(proj, deps, configure_opts, make_dir, src_dir, build_dir):
 '''
 
 projects_deps = read_projects_deps(all_deps_file)
+projects_deps = filter_projects_deps_targets()
 projects_urls = read_projects_dict(all_urls_file)
 projects_buildsystems = read_projects_dict(all_buildsystems_file)
 configure_opts = listdict()
@@ -631,6 +669,8 @@ if args.build_debug:
   content += "    --build-debug \\\n"
 if args.autoreconf_in_src_copy:
   content += "    --autoreconf-in-src-copy \\\n"
+if args.targets:
+  content += f"    --targets={shlex.quote(args.targets)} \\\n"
 content += "    $(NULL)\n"
 
 if args.autoreconf_in_src_copy:

tests/test_gen_makefile.py
@@ -64,3 +64,14 @@ def test_make_open5gs_configure_src_copy(tmp_path):
     run_cmd(["./gen_makefile.py", "-m", tmp_path, "--autoreconf-in-src-copy"], cwd=osmo_dev_path)
     run_cmd(["make", ".make.open5gs.configure"], cwd=tmp_path)
     run_make_regen_2x(tmp_path)
+
+
+def test_gen_makefile_with_targets_arg(tmp_path):
+    run_cmd(["./gen_makefile.py", "-m", tmp_path, "--targets", "trxcon,osmo-mgw"], cwd=osmo_dev_path)
+    run_cmd("grep -q '^osmocom-bb_trxcon_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_cmd("grep -q '^osmo-mgw_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_cmd("grep -q '^libosmocore_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_cmd("! grep -q '^osmo-bts_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_cmd("! grep -q '^osmo-s1gw_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_cmd("! grep -q '^open5gs_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_make_regen_2x(tmp_path)

commit 1e19eb26702ba15dbed92201e925a76e23f5255a

gen_makefile: --targets: fix subdir projects
Some projects are in subdirectories of git repositories, such as
simtrace2_host, or osmocom-bb_layer23. When filtering the targets to be
added to the Makefile, we must also add the main projects of such
projects in subdirectories, so Makefile rules like the following work:

  .make.simtrace2_host.clone: .make.simtrace2.clone

Fix for:
  make: *** No rule to make target '.make.simtrace2.clone', needed by '.make.simtrace2_host.clone'.  Stop.

Fixes: 1d13c9d7 ("gen_makefile: add --targets")
Change-Id: I136f0d9cd982653b109005aeb35885b6f015eec8

gen_makefile.py
@@ -243,6 +243,12 @@ def filter_projects_deps_targets():
           print()
           sys.exit(1)
 
+        # simtrace2_host -> simtrace2
+        if "_" in project:
+          project_main = project.split("_")[0]
+          if project_main in projects_deps and project_main not in ret:
+            queue += [project_main]
+
         deps = projects_deps[project]
         ret[project] = deps
 

tests/test_gen_makefile.py
@@ -67,10 +67,11 @@ def test_make_open5gs_configure_src_copy(tmp_path):
 
 
 def test_gen_makefile_with_targets_arg(tmp_path):
-    run_cmd(["./gen_makefile.py", "-m", tmp_path, "--targets", "trxcon,osmo-mgw"], cwd=osmo_dev_path)
+    run_cmd(["./gen_makefile.py", "-m", tmp_path, "--targets", "trxcon,osmo-mgw,simtrace2_host"], cwd=osmo_dev_path)
     run_cmd("grep -q '^osmocom-bb_trxcon_files :=' Makefile", cwd=tmp_path, shell=True)
     run_cmd("grep -q '^osmo-mgw_files :=' Makefile", cwd=tmp_path, shell=True)
     run_cmd("grep -q '^libosmocore_files :=' Makefile", cwd=tmp_path, shell=True)
+    run_cmd("grep -q '^simtrace2_files :=' Makefile", cwd=tmp_path, shell=True)
     run_cmd("! grep -q '^osmo-bts_files :=' Makefile", cwd=tmp_path, shell=True)
     run_cmd("! grep -q '^osmo-s1gw_files :=' Makefile", cwd=tmp_path, shell=True)
     run_cmd("! grep -q '^open5gs_files :=' Makefile", cwd=tmp_path, shell=True)